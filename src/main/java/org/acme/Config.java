7a3af638c811b68780fd6f2f324179a6869d70d5
Afonso Fernandes <21228942+afonsonf@users.noreply.github.com>
2023-08-03 23:34:24 +0100

    init example

---
+++ src/main/java/org/acme/Config.java
@@ -0,0 +1,27 @@
+package org.acme;
+
+import io.quarkus.logging.Log;
+import io.quarkus.runtime.Quarkus;
+import io.quarkus.runtime.StartupEvent;
+import javax.enterprise.event.Observes;
+import javax.inject.Singleton;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import java.util.Optional;
+
+@Singleton
+public class Config {
+    
+    @ConfigProperty(name="custom.env.config")
+    Optional<String> envConfig;
+    
+    void startup(@Observes StartupEvent event) {
+        if (envConfig.isPresent()) {
+            Log.info("The config is present, value: " + envConfig.get());
+        } else {
+            Log.info("The config is empty");
+        }
+
+        Quarkus.asyncExit(0);
+    }
+}
